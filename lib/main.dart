636a2cdc31e17206d192d06ce8c825c72543766f
sahilasopa <sahilasopa12@gmail.com>
2026-03-08 17:48:42 +0530

    Allow APP to resume from last checkpoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'core/theme/app_theme.dart';
-import 'core/router/app_router.dart';
+import 'package:tourist_safety_app_sih_pinnacle/core/theme/app_theme.dart';
+import 'package:tourist_safety_app_sih_pinnacle/core/router/app_router.dart';
 
 void main() {
   runApp(const ProviderScope(child: TouristSafetyApp()));
